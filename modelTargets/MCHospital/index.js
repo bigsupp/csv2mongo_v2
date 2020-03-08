224f0e82da8437f31675c6de68a56369ee2a2868
Supapong Putthayangkura <bigsupp@gmail.com>
2020-03-08 22:35:45 +0700

    add mchospital model

---
+++ modelTargets/MCHospital/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  name: String,
+  ref_code: String
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+try {
+  module.exports = mongoose.model('MCHospital', schema)
+} catch (error) {
+  module.exports = mongoose.model('MCHospital')
+}
