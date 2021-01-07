00555ba30a0286b6b8cde7d5f1c2b7bdda4ba985
bigsupp <bigsupp@gmail.com>
2021-01-07 23:27:03 +0700

    fixed if env not available

--- libs/XDCSApp.js
+++ libs/XDCSApp.js
@@ -1,14 +1,15 @@
 module.exports = (req, res, next) => {
   if(req.get('X-Custom-DCSApp-BaseURL')) {
     const prefixBaseURL = req.get('X-Custom-DCSApp-BaseURL') || ''
-    const apiBaseUrlWithoutProtocol = process.env.API_BASEURL.replace(/http:\/\//ig, '')
+    let apiBaseUrlWithoutProtocol = process.env.API_BASEURL || `http://localhost:${process.env.PORT || 8100}`
+    apiBaseUrlWithoutProtocol = apiBaseUrlWithoutProtocol.replace(/http:\/\//ig, '')
     if(apiBaseUrlWithoutProtocol.indexOf('/')===0) {
       req.app.locals.baseURL = prefixBaseURL + apiBaseUrlWithoutProtocol
     } else {
       req.app.locals.baseURL = prefixBaseURL + apiBaseUrlWithoutProtocol.replace(/^(([a-zA-Z0-9])\.?)+(:\d+)?/, '') || `${prefixBaseURL}`
     }
   } else {
-    req.app.locals.baseURL = process.env.API_BASEURL || ``
+    req.app.locals.baseURL = process.env.API_BASEURL || `http://localhost:${process.env.PORT || 8100}`
   }
   if(process.env.NODE_ENV==='development') {
     console.log(req.method, req.originalUrl)
